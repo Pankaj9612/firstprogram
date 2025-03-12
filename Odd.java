c6dc521481f7753dfac9ca5f9bb804606ae433d7
Pankaj9612 <163497297+Pankaj9612@users.noreply.github.com>
2025-03-12 23:19:51 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,14 @@
+class Odd{
+	public static void main(String args[])
+	{
+		int a=10;
+		if(a==10)
+		{
+			System.out.println("This is Even Number...");
+		}
+		else
+		{
+			System.out.println("This is Odd number");
+		}
+	}
+}
